884f1f22020e648c74ce58a256c6044f518c5e7a
Daniel Nobbe <daniellnobbe@gmail.com>
2021-01-24 17:08:35 +0100

    Added genereric order markov kernel model. Added dt_settings inside model for later use, and added this to the pretrained model files.

--- Oanda/Tests/test_predictor.py
+++ Oanda/Tests/test_predictor.py
@@ -2,10 +2,14 @@
     This file is a test script for the predictor class. Use for reference.
 """
 
-# import sys, os
-# import pathlib
-from Modules.Predictor.Predictor import Predictor
-from Modules.Training.Models import markov_kernel_1n
+import sys, os
+import pathlib
+# Specify path to look for Modules folder, based on current folder structure
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(1, path)
+from Modules.Predictor.Predictor import *
+from Modules.Training.Models import markov_kernel
+import Modules.Training.Retrieval as retrieval
 import torch
 # from Modules.Predictor.Predictor import *
 
@@ -13,19 +17,25 @@ def Test(inputt):
     # predictor = TestMarkovPredictor()
     # print(predictor.model_type)
     # print(predictor.predict())
+
+    # Data settings:
+    dt = [2*retrieval.gran_to_sec['D'], retrieval.gran_to_sec['D']]
+
     hidden_sizes = [8]
-    model = markov_kernel_1n.MarkovKernel(hidden_sizes, 1) # Example
+    model = markov_kernel.MarkovKernel(2, hidden_sizes, 1, dt_settings = None) # Example
+    
+    # Now we can find the dt values in model.dt_settings
+    
     granularity = "D"
 
-    pt_path = f"Pre-trained Models/markov1n_{hidden_sizes}_{granularity}_i1.pt"
+    # pt_path = f"Pre-trained Models/markov1n_{hidden_sizes}_{granularity}_i1.pt"
+    pt_path = "Pre-trained Models/markov2n_[8]_M1_i0.pt"
     predictor = Predictor(model, prediction_time=24, pretrained_path=pt_path)
 
-    input = torch.tensor([inputt])
+    print("dt: ", predictor.model.dt_settings)
+    input = torch.tensor([1.08, 1.09])
     prediction = predictor(input)
-
-    # print(f"Input: {input} - prediction: {prediction}")
-    
-    return input, prediction
+    print(f"Input: {input} - prediction: {prediction}")
 
 # if __name__=='__main__':
 #     main()
