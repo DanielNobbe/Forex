4f68d7ace6681d873af7a4ac1818fb10e75bcf93
Daniel Nobbe <daniellnobbe@gmail.com>
2021-02-20 19:15:47 +0100

    Also added retrieval and inference to markov kernel models

--- Oanda/Modules/Training/Models/markov_kernel.py
+++ Oanda/Modules/Training/Models/markov_kernel.py
@@ -1,5 +1,8 @@
+import torch
 import torch.nn as nn
 import numpy as np
+import Modules.Training.Retrieval as retrieval
+from pdb import set_trace
 
 def make_ordinal(n):
     '''
@@ -18,25 +21,26 @@ def make_ordinal(n):
 
 class MarkovKernel(nn.Module):
     # Simple neural network for markov kernel
-    def __init__(self, input_size, hidden_size, output_size, dt_settings=None, regression=True):
+    def __init__(self, input_size, hidden_size, output_size, dt_settings=None, regression=True, instrument=None):
         super(MarkovKernel, self).__init__()
         self.input_size = input_size
         layer_list = [
             nn.Linear(input_size, hidden_size[0]), # input size determines order of markov kernel
             nn.Tanh(),]
         for hidden_layer_index in np.arange(1, len( hidden_size ) ):
-          layer_list.append( nn.Linear( hidden_size[hidden_layer_index-1], \
+            layer_list.append( nn.Linear( hidden_size[hidden_layer_index-1], \
             hidden_size[hidden_layer_index]  ) ) # take in_features out_features into account
-          layer_list.append( nn.Tanh() ) 
+            layer_list.append( nn.Tanh() ) 
         
         layer_list.append( nn.Linear(hidden_size[-1],  output_size ) )
 
         if not regression:
-          layer_list.append(nn.Softmax())
+            layer_list.append(nn.Softmax())
         
         self.model = nn.Sequential( *layer_list ) #Unpacks list for sequential
         
         self.dt_settings = dt_settings
+        self.instrument = instrument
         self.notes = ''
         
     def __str__(self):
@@ -47,6 +51,21 @@ class MarkovKernel(nn.Module):
         output = self.model(value)
         return output
     
+    def retrieve_for_inference(self):
+        assert self.dt_settings is not None, "dt_settings required for using model. Have you loaded the model correctly?"
+        data = retrieval.retrieve_inference_data(
+            self.instrument,
+            dt = self.dt_settings,
+        ).unsqueeze(dim=0)
+        # TODO: Add soft margin to this automatically 
+        return data
+
+    def infer(self):
+        with torch.no_grad():
+            data = self.retrieve_for_inference()
+            output = self.forward(data)
+            return output[-1, -1] # Final value is prediction
+
     # def classify(self, history):
     #     classification = self.model(history[-1])
     #     return classification
