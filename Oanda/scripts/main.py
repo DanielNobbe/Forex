568c6bb75c7e69539ffa00007780780a5d729761
Daniel Nobbe <daniellnobbe@gmail.com>
2021-03-07 19:48:13 +0100

    Added usage of API_CONFIG global variable. Refactored starting of scheduler (main process now goes in infinite loop so that scheduler stays running). Scheduler will be shut down on keyboardinterrupt. Added function for building a predictor, rather than using the predictor from the test script.

--- Oanda/scripts/main.py
+++ Oanda/scripts/main.py
@@ -6,9 +6,13 @@ import sys, os
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(1, path)
 
-from libs.API.Oanda import *
+from libs.API import *
 from modules.trader.interpreter import Interpreter, VariableSafe
-from libs.API.WorkingFunctions import ReadableOutput
+from modules.training.retrieval import gran_to_sec
+from modules.trader.predictor import *
+from modules.training.models import markov_kernel
+import modules.training.retrieval as retrieval
+# from libs.API.WorkingFunctions import ReadableOutput
 
 # This does not work, because the args are not updated between runs
 from apscheduler.triggers.date import DateTrigger # Keyword 'date': use when you want to run the job just once at a certain point of time
@@ -27,31 +31,58 @@ from apscheduler.schedulers.background import BackgroundScheduler # use when you
 # =============================================================================
 # Function
 # =============================================================================
+def retrieve_current_price(access_token, accountID):
+    value = (float(PricingPricingInfo(access_token, accountID, params={"instruments": "EUR_USD"})[1]['prices'][0]['closeoutAsk'])
+                       + float(PricingPricingInfo(access_token, accountID, params={"instruments": "EUR_USD"})[1]['prices'][0]['closeoutBid']))/2
+    return value
 
-def Traden(access_token, accountID, Safe, Inter):
-    Safe.RetrievePrice(access_token, accountID)
-    Safe.Predic()
-    Inter.Trade(Safe.inputt, Safe.prediction)
+def Traden(access_token, accountID, predictor, Inter):
+    retrieve_current_price(access_token, accountID)
+    prediction = predictor()
+    Inter.Trade(Safe.inputt, prediction)
 
-def main(sched):
-    access_token = '378d83764609aa3a4eb262663b7c02ef-482ed5696d2a3cede7fca4aa7ded1c76'
-    accountID = '101-004-16661696-001'
+def build_predictor(instrument):
+    # TODO: Make this based on model config file
+    # This is an example function, should modify arguments here
+    dt = [2*gran_to_sec['D'], gran_to_sec['D']]
+    hidden_sizes = [8]
+    model = markov_kernel.MarkovKernel(2, hidden_sizes, 1, dt_settings=dt, instrument=instrument) # Example
+    pt_path = "pre-trained models/markov2n_[8]_M1_i0.pt"
+    predictor = Predictor(model, prediction_time=24, pretrained_path=pt_path)
+
+    return predictor
+
+
+def start_scheduler():
+    access_token = API_CONFIG['demo']['access_token']
+    accountID = API_CONFIG['demo']['accountID']
+
     instrument = "EUR_USD"
+
+    print("API config: ", API_CONFIG)
+
+    predictor = build_predictor(instrument)
     
     print(TransactionsTransactionsSinceID(access_token, accountID, params={"id": "20"})[0])
 
     Safe = VariableSafe()
     Inter = Interpreter(access_token, accountID, 'EUR_USD')
-    
-    sched.add_job(Traden, args=(access_token, accountID, Safe, Inter), trigger='interval', seconds=5)  
+
+    sched = BackgroundScheduler()
+    sched.add_job(Traden, args=(access_token, accountID, predictor, Inter), trigger='interval', seconds=5)  
     sched.start()
 
-if __name__ == "__main__":
+    return sched
+
+def main():
+    start_scheduler()
     try:
-        print("Previous scheduler has been shut down")
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("Shutting down scheduler.")
         sched.shutdown()
-    except:
-        print("No scheduler running")
-        sched = BackgroundScheduler()
-        main(sched)
+
+if __name__ == "__main__":
+    main()
 
