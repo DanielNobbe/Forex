3dc7d80de08c99cbcc7880fbbd4090c19e97f695
Daniel Nobbe <daniellnobbe@gmail.com>
2021-03-07 20:15:48 +0100

    Refactored interpreter to have funtionality split more across functions. One method handles risk calculation now, while another handles the trading strategy. These functions can now be easily rewritten. Exceptions in the order are also handled differently now, showing the exact exception.

--- Oanda/modules/trader/interpreter/interpreter.py
+++ Oanda/modules/trader/interpreter/interpreter.py
@@ -32,26 +32,40 @@ class Interpreter():
     #     maximum_position = min(cash, 0.25*portfoliosize-positions[instrument])
     #     return maximum_position
     
-    def Units(self, input, prediction):
+    def update_value(self, input):
         # NAV = float(AccountSummary(self.access_token, self.accountID)[1]['account']['NAV'])
         self.size = float(PositionsPositionDetails(self.access_token, self.accountID, instrument=self.instrument)[1]['position']['long']['units']) + float(PositionsPositionDetails(self.access_token, self.accountID, instrument=self.instrument)[1]['position']['short']['units'])
-        price = input
-        
-        upperbound = 20000 # 1000 euro
+        self.owned_value = self.size*price
+        self.price = input
+        # return value
         # max_trade = 0.0005*NAV # ong 10 euro
         # print(size*price)
         
-        if prediction > input:
-            if self.size*price < upperbound:
-                self.units = int(round(upperbound/price))
+    def check_risk(self, action, amount=None):
+        if amount is None:
+            if action == 'buy':
+                amount = int((self.upperbound-self.owned_value)/self.price) # A unit is 1 dollar here? TODO:
+            elif action == 'sell':
+                amount = int((self.lowerbound-self.owned_value)/self.price)
             else:
-                self.units = None
-        else:
-            if self.size*price > -upperbound:
-                self.units = -int(round(upperbound/price))
+                raise ValueError(f"action should be buy or sell, got {action}")
+        if action == 'buy':
+            if self.owned_value + amount <= self.upperbound:
+                # Allowed to buy up to upper bound
+                return True, amount
             else:
-                self.units = None
+                # Trying to buy too much
+                return False, amount
+        elif action == 'sell':
+            if self.owned_value + amount >= self.lowerbound:
+                # Allowed to buy down to lowerbound
+                return True, amount
+            else:
+                return False, amount
+
+
         
+    
 # =============================================================================
 #         if prediction > input:
 #             if size*price < upperbound:
@@ -64,22 +78,58 @@ class Interpreter():
 #             else:
 #                 self.units = None
 # =============================================================================
-                    
+    def prepare_trade(self, input, prediction):
+        # Determines whether to buy or sell
+        # based on prediction, and checks this with risk management
+        # TODO: Should implement more complex strategies here
+        # TODO: Add max amount to buy here, use config file
+        if prediction > input:
+            # Price will go up, so we should buy
+            amount = 10
+            allowed, amount_ret = self.check_risk('buy', amount)
+            assert amount == amount_ret, "Mistake in check_risk function"
+            if allowed:
+                return 'buy', amount
+            else:
+                return False, amount
+        elif prediction < input:
+            # Sell, short or hold?
+            amount = -10
+            allowed, amount_ret = self.check_risk('buy', amount)
+            assert amount == amount_ret, "Mistake in check_risk function"
+            if allowed:
+                return 'sell', amount
+            else:
+                return False, amount
+
+    def to_units(self, amount):
+        # TODO: Uses dollars as units?
+        return amount / self.price
+
     def Trade(self, input, prediction):
         
-        self.Units(input, prediction)
-            
+        self.update_value(input)
+        self.upperbound = 20000.
+        self.lowerbound = -20000. # when going short
+        
+        buy_or_sell, amount = self.prepare_trade(input, prediction)
+        if buy_or_sell:
+            units = to_units(amount)
+        else:
+            print(f"Can not buy or sell {amount} of {self.instrument}. Returning..")
+            return
+
         data = MarketOrder
-        data['order']['units'] = self.units
+        data['order']['units'] = units
         data['order']['instrument'] = self.instrument
         data['order']['timeInForce'] = "FOK"
         
         FilterDict(data)
         
         print(ReadableOutput(data))
-        
         try:
             OrdersOrderCreate(self.access_token, self.accountID, data=data)
             print("Bought ", self.units, " ", self.instrument, " value of trade: ", self.size*input)
-        except:
+        except Exception as e:
             print("Order was NOT accepted, value of trade: ", self.size*input)
+            print("Error: ", e)
