651b1c1f0d6ddd4e78895be8578d48454a9bf6f0
Daniel Nobbe <daniellnobbe@gmail.com>
2021-05-30 16:32:44 +0200

    Added documentation to interpreter folder

--- Oanda/modules/trader/interpreter/__init__.py
+++ Oanda/modules/trader/interpreter/__init__.py
@@ -1,3 +1,4 @@
 from .interpreter import Interpreter
 from .safe import VariableSafe
+## Following line imports all public functions in this folder
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
