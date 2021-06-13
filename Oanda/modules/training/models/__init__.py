c8eaaae6ef8623dfa1be236bc6fe562cc9f19081
Daniel Nobbe <daniellnobbe@gmail.com>
2021-06-13 15:41:20 +0200

    Changed RNN.py to rnn.py and fixed references

--- Oanda/modules/training/models/__init__.py
+++ Oanda/modules/training/models/__init__.py
@@ -5,7 +5,7 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.startswith('_')]
 
 from .markov_kernel import MarkovKernel
-from .RNN import CandleLSTM
+from .rnn import CandleLSTM
 
 ARCHITECTURES = {
     "markov_kernel":    MarkovKernel,
