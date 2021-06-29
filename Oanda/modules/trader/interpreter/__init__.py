d07ee9342f4f33759acb7be38d8707be27b84339
DanielNobbe <42846160+DanielNobbe@users.noreply.github.com>
2021-06-29 21:08:03 +0200

    Update __init__.py
    Removed import from safe.py, which does not exist anymore

--- Oanda/modules/trader/interpreter/__init__.py
+++ Oanda/modules/trader/interpreter/__init__.py
@@ -1,4 +1,3 @@
 from .interpreter import Interpreter
-from .safe import VariableSafe
 ## Following line imports all public functions in this folder
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
